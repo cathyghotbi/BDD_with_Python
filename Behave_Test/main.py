469459e024f157aeae04fcf8f3732b25b0bff482
cathyghotbi <cathyghotbi@yahoo.com>
2024-01-09 18:02:14 +0100

    Create main.py

---
+++ Behave_Test/main.py
@@ -0,0 +1,12 @@
+import behave
+from behave import __main__ as runner
+import addition
+if __name__ == '__main__':
+
+# C:\Users\Cathy\PycharmProjects\Behave_Test\feature
+feature_location = '../Behave_Test/feature'
+var = runner.main(feature_location)
+
+# REFERENCES:
+# https://www.techlistic.com/2023/05/python-behave-tutorial.html
+# https://www.c-sharpcorner.com/article/bdd-with-python/
